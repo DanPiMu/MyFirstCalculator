c87c63891866051698e979cf3dd28b837d2f3da5
Daniel Pilligua <alejandropilliguamurillo@gmail.com>
2023-05-31 14:16:59 +0200

    Added initial architecture

---
+++ MyCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class Calculator
+    {
+        public int Sum(int sum1, int sum2)
+        {
+            return sum1 + sum2;
+        }
+        public int Substract(int sum1, int sum2)
+        {
+            return sum1 - sum2;
+        }
+        public int Multiply(int sum1, int sum2)
+        {
+            return sum1 * sum2;
+        }
+        public int Divide(int sum1, int sum2)
+        {
+            return sum1 / sum2;
+        }
+    }
+}
---
+++ MyCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Sum(2, 2) == 4);
+            
+        }
+
+        [TestMethod()]
+        public void SubstractTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Substract(8, 2) == 6);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Multiply(2, 3) == 6);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            var calculator = new Calculator();
+            Assert.IsTrue(calculator.Divide(2, 2) == 1);
+        }
+    }
+}
