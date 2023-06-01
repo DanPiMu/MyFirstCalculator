247af9f55206eb876e66ff4ee6bc0efaf97e8d5d
Daniel Pilligua <alejandropilliguamurillo@gmail.com>
2023-06-01 14:08:48 +0200

    Added new configuration closes #1

---
+++ MyCalculator/DotNetConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public class DotNetConfiguration
+    {
+        public static string GetConfiguration() {
+            return "new config";
+        }
+    }
+}
